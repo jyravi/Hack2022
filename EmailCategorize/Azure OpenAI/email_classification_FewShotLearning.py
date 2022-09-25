65ae910a4a90a3a94f629486a914dd9b3f102499
jyravi <56615890+jyravi@users.noreply.github.com>
2022-09-25 08:06:00 +0530

    Add files via upload

---
+++ EmailCategorize/Azure OpenAI/email_classification_FewShotLearning.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os
+import openai
+
+##read the csv file (sample is provided. You may create your own as well.)
+df = pd.read_csv('fewshotemail.csv')
+
+text=[]
+text=df['Content'].tolist()
+label=[]
+label=df['actual'].tolist()
+
+
+###OpenAi api on Azure credentials
+openai.api_type = "azure"
+###paste your resource name
+openai.api_base = "https://[resourcename].openai.azure.com/"
+openai.api_version = "2022-06-01-preview"
+### Paste your key
+openai.api_key = "##############"
+
+
+key=[]
+
+description="This is an email classifier"
+key1="message"
+key2="Category"
+key=[key1,key2]
+
+####Paste your query text  and construct the openai query####
+
+query_text=" "
+
+def get_prompt(description,key, text, label, query_text):
+    prompttext=''
+    for i in range(len(text)):
+        ptext=''.join(str(key[0])+":"+str(text[i])+" "+str(key[1])+":"+ str(label[i]))
+        prompttext+=f"{ptext}\n"
+    
+    query1=f"{key1}: {query_text} {key2}: "
+    prompt = (f"{description}\n {prompttext} {query1}")
+
+    return prompt
+
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=get_prompt(description,key, text, label, query_text),
+  temperature=0,
+  max_tokens=64,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=None
+)
+
+print(response['choices'])
---
+++ EmailCategorize/Azure OpenAI/email_classification_OpenAI.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import openai
+
+###Loading data 
+df=pd.read_csv('test_email.csv')
+content=df['Content'].tolist()
+
+
+###Define your own categories
+categories=["Logistics","Personal","Confirmation","Follow Up"]
+
+###OpenAi api on Azure credentials
+openai.api_type = "azure"
+###paste your resource name
+openai.api_base = "https://[resourcename].openai.azure.com/"
+openai.api_version = "2022-06-01-preview"
+### Paste your key
+openai.api_key = "##############"
+
+###Construct the prompt
+
+def create_prompt(categories,query_text):  
+    description=f"Classifiy the email into any one of the following categories :{categories}"
+    prompt = (f"{description}\n {query_text}\n")
+    return prompt
+#print(get_prompt(categories, query_text))
+
+### Make the openai api call
+
+query_text=content[5]
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=create_prompt(categories,query_text),
+  temperature=0,
+  max_tokens=64,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=None
+)
+print(response['choices'])
+
+print(response['choices'][0]['text'])
+
